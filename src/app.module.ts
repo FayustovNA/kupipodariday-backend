91785cca5b713a9304f34cc883f5b1c55c82de2e
Nikolay Fayustov <n.a.fayustov@gmail.com>
2023-08-08 21:32:18 +0300

    1-st

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { OffersModule } from './offers/offers.module';
+import { UsersModule } from './users/users.module';
+import { WishesModule } from './wishes/wishes.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
 
 @Module({
-  imports: [],
+  imports: [OffersModule, UsersModule, WishesModule, WishlistsModule],
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
